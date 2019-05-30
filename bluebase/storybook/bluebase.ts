0ce39bc19758dada8f4305c415767032a6409fc8
Muhammad Husnain Naseer <mhusnainnaseer786@gmail.com>
2019-05-31 00:41:52 +0500

    fix(SkeletonLoader): .npmrc added

--- bluebase/storybook/bluebase.ts
+++ bluebase/storybook/bluebase.ts
@@ -1,11 +1,9 @@
 import { BootOptions } from '@bluebase/core';
 import commonBootOptions from '../common/bluebase';
 import deepmerge from 'deepmerge';
-// import Plugin from '../../src';
-// import {Router} from '@bluebase/plugin-react-router';
+
 // TODO: Only for evaluation, remove this
 import DummyPlugin from './sample';
-import plugin from  "../../src/index"
 
 /**
  * Add your platform specific configs here. 
@@ -16,10 +14,7 @@ const bootOptions: Partial<BootOptions> = {
 
 	plugins: [
 		// TODO: Only for evaluation, remove this
-		plugin,
 		DummyPlugin
-		// Plugin,
-		// Router
 	],
 	// config: {
 
