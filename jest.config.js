67577fd0df34593267758fe461d220cb4c680204
Abubakar Saddique <abubakar.mani1@gmail.com>
2019-06-10 18:47:50 +0500

    chore(jest ): upadate

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ const configs = require("@bluebase/code-standards/jest.config");
 
 module.exports = Object.assign(configs, {
 	// setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
-	testRegex: undefined,
+	testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
   "preset": "jest-expo",
   "transformIgnorePatterns": [
     "node_modules/(?!((jest-)?react-native|react-clone-referenced-element|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|sentry-expo|native-base))"
